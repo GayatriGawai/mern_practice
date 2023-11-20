62eaf345c9c90299a205fce9dde19b551927dab0
insnapsys <gayatri.g.insynapsys@gmail.com>
2023-11-20 12:16:04 +0530

    Created the user model 'User.js'

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express'); //for simple express server
+
 //Here we established the connection between two files
 const connectDB = require('./config/db');
 const app = express();
 
 //connect database
 connectDB();
-app.get('/', (req, res) => res.send('API Running')); //single end point for check
-// the get function has two parameters path(to get the router) and the request and response
+
+app.get('/', (req, res) => res.send('API Running'));
+/* single end point for check
+- the get function has two parameters path(to get the router) and the request and response
+- send() is used to pass the message to the browser */
 
 //Defined routes for each js file
+/*Use the router exported from ./routes/api/users for any requests 
+whose path starts with '/api/users'. */
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
