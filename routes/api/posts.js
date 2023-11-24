a7230084c37f6a16b467bb1715515c7050f1f85e
insnapsys <gayatri.g.insynapsys@gmail.com>
2023-11-24 17:35:39 +0530

    POST API to coment on a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -154,5 +154,37 @@ router.put('/unlike/:id', auth, async (req, res) => {
         return res.status(500).send('Server error');
     }
 });
+
+//@route     POST api/posts/comment
+//@desc      Comment on a post
+//@access    Private
+router.post(
+    '/comment/:id',
+    [auth, [check('text', 'Text is required').not().isEmpty()]],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        try {
+            const user = await User.findById(req.user.id).select('-password');
+            const post = await Post.findById(req.params.id);
+            const newComment = {
+                text: req.body.text,
+                name: user.name,
+                avatar: user.avatar,
+                user: req.user.id,
+            };
+            post.comments.unshift(newComment);
+
+            await post.save();
+
+            res.json(post.comments);
+        } catch (err) {
+            console.error(err.message);
+            return res.status(500).send('Server Error');
+        }
+    }
+);
 //to export the route we created
 module.exports = router;
