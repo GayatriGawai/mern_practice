f44be0e79fb4744d5b869d8fb91496f726e04082
insnapsys <gayatri.g.insynapsys@gmail.com>
2023-11-24 16:34:25 +0530

    PUT API to unlike the post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -123,6 +123,36 @@ router.put('/like/:id', auth, async (req, res) => {
     }
 });
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//@route     PUT api/posts/unlike/:id
+//@desc      Unlike a post
+//@access    Private
 
+router.put('/unlike/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        // if (!post) {
+        //     return res.status(404).json({ msg: 'Post not found' });
+        // }
+
+        //check if the post is already been liked
+        if (
+            post.likes.filter((like) => like.user.toString() === req.user.id)
+                .length === 0
+        ) {
+            return res.status(400).json({ msg: 'Post has not yet been liked' });
+        }
+
+        //get remove the index
+        const removeIndex = post.likes.map((like) =>
+            like.user.toString().indexOf(req.user.id)
+        );
+        post.likes.splice(removeIndex, 1);
+        await post.save();
+        res.json(post.likes);
+    } catch (err) {
+        console.error(err.message);
+        return res.status(500).send('Server error');
+    }
+});
 //to export the route we created
 module.exports = router;
